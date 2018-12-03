c8b7313a42ac0c9553d2bed2085410a500932221
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-12-03 12:27:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {} 
+  names_array = data[:gender][:male].concat(data[:gender][:female])
+  names_array.each do |name|
+     
+  end
 end
+
+data[:color][:]
